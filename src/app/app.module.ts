d7c3db5a6bbf089dcd35196b0404e0988c0d8c99
NoahVersace <nsa109710@iet-gibb.ch>
2019-11-17 18:34:21 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GameComponent } from './game/game.component';
+import { StartGameComponent } from './game/start-game/start-game.component';
+import { EndGameComponent } from './game/end-game/end-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    GameComponent,
+    StartGameComponent,
+    EndGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/game/end-game/end-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-game',
+  templateUrl: './end-game.component.html',
+  styleUrls: ['./end-game.component.scss']
+})
+export class EndGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
