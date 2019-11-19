5d6f51d5cd70018b7c76c6abf8e702835087a787
Ivan Quintela <ivuquintela@gmail.com>
2019-11-19 20:42:30 +0100

    routing chances

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { GameComponent } from './game/game.component';
-import { StartGameComponent } from './game/start-game/start-game.component';
-import { PlayComponent } from './game/play/play.component';
-import { EndGameComponent } from './game/end-game/end-game.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { GameComponent } from "./game/game.component";
+import { StartGameComponent } from "./game/start-game/start-game.component";
+import { PlayComponent } from "./game/play/play.component";
+import { EndGameComponent } from "./game/end-game/end-game.component";
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent },
-  { path: 'game', component: GameComponent,
+  { path: "", component: LandingPageComponent },
+  {
+    path: "game",
+    component: GameComponent,
     children: [
-      { path: '', component: PlayComponent },
-      { path: 'start', component: StartGameComponent },
-      { path: 'end', component: EndGameComponent }
-    ] }
+      { path: "", component: StartGameComponent },
+      { path: "play", component: PlayComponent },
+      { path: "end", component: EndGameComponent }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
