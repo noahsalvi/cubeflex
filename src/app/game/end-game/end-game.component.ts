7aa1b23d1911a16358d3d7156835eb6320f2864a
NoahVersace <noahsalvi@me.com>
2019-11-19 21:50:04 +0100

    add end-game template

--- src/app/game/end-game/end-game.component.ts
+++ src/app/game/end-game/end-game.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-end-game',
-  templateUrl: './end-game.component.html',
-  styleUrls: ['./end-game.component.scss']
+	selector: "app-end-game",
+	templateUrl: "./end-game.component.html",
+	styleUrls: ["./end-game.component.scss"]
 })
 export class EndGameComponent implements OnInit {
+	time: number;
 
-  constructor() { }
+	constructor() {
+		this.time = 5;
+	}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
