a8617a88a59025550e33aa9a4eee28489b04943a
NoahVersace <noahsalvi@me.com>
2019-11-19 20:48:59 +0100

    rename Component play-game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from "@angular/router";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { GameComponent } from "./game/game.component";
 import { StartGameComponent } from "./game/start-game/start-game.component";
-import { PlayComponent } from "./game/play/play.component";
 import { EndGameComponent } from "./game/end-game/end-game.component";
+import { PlayGameComponent } from "./game/play-game/play-game.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
@@ -13,7 +13,7 @@ const routes: Routes = [
     component: GameComponent,
     children: [
       { path: "", component: StartGameComponent },
-      { path: "play", component: PlayComponent },
+      { path: "play", component: PlayGameComponent },
       { path: "end", component: EndGameComponent }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { GameComponent } from './game/game.component';
-import { StartGameComponent } from './game/start-game/start-game.component';
-import { EndGameComponent } from './game/end-game/end-game.component';
-import { PlayComponent } from './game/play/play.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { GameComponent } from "./game/game.component";
+import { StartGameComponent } from "./game/start-game/start-game.component";
+import { EndGameComponent } from "./game/end-game/end-game.component";
+import { PlayGameComponent } from './game/play-game/play-game.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +16,10 @@ import { PlayComponent } from './game/play/play.component';
     GameComponent,
     StartGameComponent,
     EndGameComponent,
-    PlayComponent
+    PlayGameComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
