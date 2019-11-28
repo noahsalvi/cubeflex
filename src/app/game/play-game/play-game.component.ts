d022be4ceac9665486523f9f94de74fb5633f023
iqu109733 <iqu109733@iet-gibb.ch>
2019-11-28 23:08:12 +0100

    begin play-game

--- src/app/game/play-game/play-game.component.ts
+++ src/app/game/play-game/play-game.component.ts
@@ -7,21 +7,52 @@ import { GameService } from "src/app/game.service";
 	styleUrls: ["./play-game.component.scss"]
 })
 export class PlayGameComponent implements OnInit, AfterViewInit {
+	rand: number;
+	interval;
+	timeLeft: number;
+
 	constructor(private gameService: GameService) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		for (let i = 1; i <= 2; i++) {
+			document
+				.getElementById("c-" + i)
+				.addEventListener("click", () => this.checkCube("c-" + i));
+		}
+	}
 
 	ngAfterViewInit() {
-		document.getElementById("c-1").addEventListener("click", this.test);
-	}
-	chooseCard() {
-		let randNr = Math.floor(Math.random() * 6) + 1;
-		console.log(randNr);
+		this.chooseCube();
+		this.setActiv();
+		this.startCountdown().then(() => console.log("fertig"));
 	}
 
-	test() {
-		document
-			.getElementById("c-1")
-			.addEventListener("click", this.chooseCard);
+	chooseCube() {
+		let randNr = Math.floor(Math.random() * 12) + 1;
+		this.rand = randNr;
+	}
+
+	setActiv() {
+		document.getElementById("c-" + this.rand).classList.add(".activ");
+	}
+
+	checkCube(id: String) {
+		if (id == "c-" + this.rand) {
+			this.countdown = 0;
+		} else {
+			this.gameService.gameover = true;
+		}
+	}
+
+	async startCountdown() {
+		this.timeLeft = 2;
+		this.interval = setInterval(() => {
+			if (this.timeLeft > 0) {
+				this.timeLeft--;
+			} else {
+				this.timeLeft = 60;
+			}
+			console.log(this.timeLeft);
+		}, 1000);
 	}
 }
