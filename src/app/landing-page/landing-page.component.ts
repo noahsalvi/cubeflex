7a6fe4c6f86cb1332287746ee236b051c15ed960
iqu109733 <iqu109733@iet-gibb.ch>
2019-12-01 00:05:46 +0100

    begin footer

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Md5 } from "@angular/ts-md5/dist/md5";
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+	selector: "app-landing-page",
+	templateUrl: "./landing-page.component.html",
+	styleUrls: ["./landing-page.component.scss"]
 })
 export class LandingPageComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-    
-  }
-
-
+	ngOnInit() {}
 }
